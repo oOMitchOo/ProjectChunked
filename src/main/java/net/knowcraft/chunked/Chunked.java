b562c7dd121787d24968f317386456b5545e9beb
oOMitchOo <mitch@knowcraft.net>
2016-10-03 22:56:14 +0200

    skeleton of the mod

---
+++ src/main/java/net/knowcraft/chunked/Chunked.java
@@ -0,0 +1,12 @@
+package net.knowcraft.chunked;
+
+import net.minecraftforge.fml.common.Mod;
+import net.knowcraft.chunked.reference.Reference;
+
+/**
+ * Created by oOMitchOo on 03.10.2016.
+ */
+
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.MOD_VERSION)
+public class Chunked {
+}
---
+++ src/main/java/net/knowcraft/chunked/reference/Reference.java
@@ -0,0 +1,12 @@
+package net.knowcraft.chunked.reference;
+
+/**
+ * Created by oOMitchOo on 03.10.2016.
+ */
+public class Reference {
+    public static final String MOD_ID = "chunked";
+    public static final String MOD_NAME = "Chunked";
+    public static final String MC_VERSION = "1.10.2";
+    public static final String MOD_VERSION = MC_VERSION+"-"+"0.5";
+
+}
